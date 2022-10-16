4731a122127a8b723c6854abf81d36fe2c05f30b
kacamata09 <muh.ansharazhari@gmail.com>
2022-10-16 13:13:32 +0700

    Ini adalah belajar express

---
+++ 01_hello_world.js
@@ -0,0 +1,14 @@
+// bismillah coba express
+
+// anshar
+
+express = require('express')
+
+app = express()
+
+app.listen(2000, console.log('Ini adalah server express pertamaku, anshar'))
+
+app.route('/')
+    .get((requ, resp) => {
+        resp.send('Hello Dunia ini adalah world')
+    }) 
